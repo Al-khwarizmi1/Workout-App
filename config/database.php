1622c946e91593af9a5721295494b4ac49d8d341
Ethan3600 <Ethan3600@gmail.com>
2016-06-18 18:22:31 -0400

    test

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'workout'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '#11bulldogs'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
